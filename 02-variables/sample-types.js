de3b51892eb0a086466a50b07353f6e66ca5e919
Oscar Cheng <oscarcheng312@gmail.com>
2025-05-22 23:14:39 +0800

    variables for ts

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,8 @@
+"use strict";
+let lastName = "Oscar";
+let firstName = "Cheng";
+let grade = 88.6;
+let found = true;
+console.log("Hi my lastName is " + lastName);
+console.log("My final grades is " + grade);
+console.log(`Hi ${lastName} ${firstName}`);
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,8 @@
+let lastName: String = "Oscar";
+let firstName: String = "Cheng";
+let grade: number = 88.6;
+let found: boolean = true;
+
+console.log("Hi my lastName is " + lastName);
+console.log("My final grades is " + grade);
+console.log(`Hi ${lastName} ${firstName}`);
